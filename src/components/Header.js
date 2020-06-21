c7a7788a99f22e06fd399f6f698927ad9b900281
Suhail <SuhailArshad1122@gmail.com>
2020-06-22 01:32:32 +0500

    created expense tracker

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <h3>
+            Expense Tracker 
+        
+            ** By Suhail Arshad **
+            <img src="PakFlag.png" width="50px" ></img>
+        </h3>
+    )
+}
+
+
+
