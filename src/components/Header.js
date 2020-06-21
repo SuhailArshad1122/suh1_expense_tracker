82d30952dfb9523ddf829c5625bd6451eecf3f84
Suhail <SuhailArshad1122@gmail.com>
2020-06-22 02:25:41 +0500

    Pak Flag removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ export const Header = () => {
         <h3>
             Expense Tracker 
             ** By Suhail Arshad **
-            <img src="PakFlag.png" width="50px" alt="flag"/>
         </h3>
     )
 }
